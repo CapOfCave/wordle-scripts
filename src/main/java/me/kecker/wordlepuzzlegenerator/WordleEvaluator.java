9b27405f7d2267b37d7fb8e7e05f6e3f245b285c
Lars Kecker <lars.kecker2009@gmail.com>
2022-05-24 20:21:50 +0200

    create compact evaluation

--- src/main/java/me/kecker/wordlepuzzlegenerator/WordleEvaluator.java
+++ src/main/java/me/kecker/wordlepuzzlegenerator/WordleEvaluator.java
@@ -4,11 +4,16 @@ import java.util.Locale;
 
 public class WordleEvaluator {
 
-    private static final int WORD_LENGTH = 5;
+    public static final int WORD_LENGTH = 5;
+    public static final int MAX_RESULT_BIT_LENGTH = WORD_LENGTH * 2;
+    public static final int MAX_RESULT_VALUE = (int) Math.pow(2, MAX_RESULT_BIT_LENGTH) - 1;
+    public static final int MAX_RESULT_VALUE_COMPACT = (int) Math.pow(3, WORD_LENGTH) - 1;
+
+
 
     /**
      * Return an integer that represents a 10 digit binary number.
-     *
+     * <p>
      * The first 5 bits indicate if the letter was green, the next 5 bits indicate if the letter was yellow.
      */
     public static int evaluate(String guess, String solution) {
@@ -27,23 +32,49 @@ public class WordleEvaluator {
         int green = 0;
         for (int i = 0; i < WORD_LENGTH; i++) {
             if (guess.charAt(i) == solution.charAt(i)) {
-                green += (1 << (WORD_LENGTH - i - 1));
+                green += at(i);
                 remainingOccurencesOfMove[guess.charAt(i) - 'a']--;
             }
         }
 
         int yellow = 0;
         for (int i = 0; i < WORD_LENGTH; i++) {
-            if ((green & (1 << (WORD_LENGTH - i - 1))) != 0) continue;
+            if ((green & at(i)) != 0) continue;
 
             char guessed = guess.charAt(i);
             if (remainingOccurencesOfMove[guessed - 'a'] > 0) {
-                yellow += (1 << (WORD_LENGTH - i - 1));
+                yellow += at(i);
                 remainingOccurencesOfMove[guessed - 'a']--;
             }
         }
 
         return (green << WORD_LENGTH) + yellow;
     }
+
+
+    /**
+     * Create an evaluation and store it in a base 3 integer
+     */
+    public static int evaluateCompact(String guess, String solution) {
+        int evaluation = evaluate(guess, solution);
+        int evaluationCompact = 0;
+        int green = evaluation >> WORD_LENGTH;
+        int yellow = evaluation & 0b11111;
+
+        for (int i = 0; i < WORD_LENGTH; i++) {
+            evaluationCompact *= 3;
+            int tmp = (green & at(i)) >> (WORD_LENGTH - i - 1);
+            // can only have values 1 or 0
+            int isGreen = (green & at(i)) >> (WORD_LENGTH - i - 1);
+            int isYellow = (yellow & at(i)) >> (WORD_LENGTH - i - 1);
+            evaluationCompact += 2 * isGreen + isYellow;
+        }
+        return evaluationCompact;
+    }
+
+    private static int at(int i) {
+        return 1 << (WORD_LENGTH - i - 1);
+    }
+
 
 }
--- src/test/java/me/kecker/wordlepuzzlegenerator/WordleEvaluatorTest.java
+++ src/test/java/me/kecker/wordlepuzzlegenerator/WordleEvaluatorTest.java
@@ -7,37 +7,72 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class WordleEvaluatorTest {
 
     @Test
-    void testOneGreenLetter() {
+    void testEvaluateOneGreenLetter() {
         int evaluation = WordleEvaluator.evaluate("thill", "trace");
         String evaluationBinary = String.format("%10s", Integer.toBinaryString(evaluation)).replace(' ', '0');
         assertEquals("1000000000", evaluationBinary);
     }
 
     @Test
-    void testOneYellowLetter() {
+    void testEvaluateOneYellowLetter() {
         int evaluation = WordleEvaluator.evaluate("third", "brace");
         String evaluationBinary = String.format("%10s", Integer.toBinaryString(evaluation)).replace(' ', '0');
         assertEquals("0000000010", evaluationBinary);
     }
 
     @Test
-    void testMultipleLettersCorrect() {
+    void testEvaluateMultipleLettersCorrect() {
         int evaluation = WordleEvaluator.evaluate("theft", "tests");
         String evaluationBinary = String.format("%10s", Integer.toBinaryString(evaluation)).replace(' ', '0');
         assertEquals("1000000101", evaluationBinary);
     }
 
     @Test
-    void testNoSecondYellow() {
+    void testEvaluateNoSecondYellow() {
         int evaluation = WordleEvaluator.evaluate("creek", "feuds");
         String evaluationBinary = String.format("%10s", Integer.toBinaryString(evaluation)).replace(' ', '0');
         assertEquals("0000000100", evaluationBinary);
     }
 
     @Test
-    void testNoYellowIfGreen() {
+    void testEvaluateNoYellowIfGreen() {
         int evaluation = WordleEvaluator.evaluate("seeks", "brent");
         String evaluationBinary = String.format("%10s", Integer.toBinaryString(evaluation)).replace(' ', '0');
         assertEquals("0010000000", evaluationBinary);
+    }
+
+    @Test
+    void testEvaluateCompactOneGreenLetter() {
+        int evaluation = WordleEvaluator.evaluateCompact("thill", "trace");
+        String evaluationString = String.format("%5s", Integer.toString(evaluation, 3)).replace(' ', '0');
+        assertEquals("20000", evaluationString);
+    }
+
+    @Test
+    void testEvaluateCompactOneYellowLetter() {
+        int evaluation = WordleEvaluator.evaluateCompact("third", "brace");
+        String evaluationString = String.format("%5s", Integer.toString(evaluation, 3)).replace(' ', '0');
+        assertEquals("00010", evaluationString);
+    }
+
+    @Test
+    void testEvaluateCompactMultipleLettersCorrect() {
+        int evaluation = WordleEvaluator.evaluateCompact("theft", "tests");
+        String evaluationString = String.format("%5s", Integer.toString(evaluation, 3)).replace(' ', '0');
+        assertEquals("20101", evaluationString);
+    }
+
+    @Test
+    void testEvaluateCompactNoSecondYellow() {
+        int evaluation = WordleEvaluator.evaluateCompact("creek", "feuds");
+        String evaluationString = String.format("%5s", Integer.toString(evaluation, 3)).replace(' ', '0');
+        assertEquals("00100", evaluationString);
+    }
+
+    @Test
+    void testEvaluateCompactNoYellowIfGreen() {
+        int evaluation = WordleEvaluator.evaluateCompact("seeks", "brent");
+        String evaluationString = String.format("%5s", Integer.toString(evaluation, 3)).replace(' ', '0');
+        assertEquals("00200", evaluationString);
     }
 }
